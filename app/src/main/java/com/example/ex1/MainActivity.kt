7946ee3845a1707c5c821b3cebaf2c0a5e33b679
gilzada <gilzada8@gmail.com>
2020-04-04 21:52:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ex1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ex1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            task.text = editText.text
+            editText.text.clear()
+        }
+    }
+
+}
